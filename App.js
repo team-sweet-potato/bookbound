e55385f5e5124a92e57f9e74f4f9139571411a79
Sarah Perrone <sspillman21@gmail.com>
2022-07-05 16:25:34 -0400

    created login component

--- App.js
+++ App.js
@@ -1,32 +1,17 @@
-import React from "react";
-import {Buttons} from "./components/Buttons";
-import { StyleSheet, View, Image} from "react-native";
+import React from 'react';
+// 1. import `NativeBaseProvider` component
+import { NativeBaseProvider, Box, Stack } from 'native-base';
+import { NavigationContainer, TabRouter } from "@react-navigation/native";
+import { Login } from './components/Login';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Image
-        style={styles.tinyLogo}
-        source={require("./assets/bookboundtestlogo.png")}
-      ></Image>
-      <Buttons />
-    </View>
+    <NativeBaseProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={Login} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#f9f9f7",
-    padding: 70,
-    flex: 1,
-    alignItems: "center",
-  },
-  tinyLogo: {
-    width: 200,
-    height: 100,
-  },
-  logo: {
-    width: 66,
-    height: 58,
-  },
-});
