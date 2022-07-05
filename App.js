0be758f7c092508025c2eefd1778849b64e380cd
Noelle Mittelstaedt <noellemittelstaedt@gmail.com>
2022-07-05 13:24:47 -0400

    First commit for 'log-in page'

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
 import React from "react";
-import { NativeBaseProvider, Box } from "native-base";
+import Buttons from "./components/Buttons";
+import { StyleSheet, View, Image } from "react-native";
 
 export default function App() {
   return (
-    <NativeBaseProvider>
-      <Box>Hello world</Box>
-    </NativeBaseProvider>
+    <View style={styles.container}>
+      <Image
+        style={styles.tinyLogo}
+        source={require("./assets/bookboundtestlogo.png")}
+      ></Image>
+      <Buttons />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#f9f9f7",
+    padding: 70,
+    flex: 1,
+    alignItems: "center",
+
+  },
+  tinyLogo: {
+    width: 200,
+    height: 100,
+  },
+  logo: {
+    width: 66,
+    height: 58,
+  },
+
+});
