cc7a8cdaf872d48ceef521da35ee7c3b4906579c
Corinna Hamilton <corinnahamilton01@gmail.com>
2022-07-05 15:23:56 -0400

    created base components

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { NativeBaseProvider, Box } from "native-base";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
     <NativeBaseProvider>
-      <Box>Hello world</Box>
+      <NavigationContainer>
+        <Box>Hello world</Box>
+      </NavigationContainer>
     </NativeBaseProvider>
   );
 }
