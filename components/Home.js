4b6ef9a754fe2213126954ddd1e705bc745964bf
Corinna Hamilton <corinnahamilton01@gmail.com>
2022-07-06 01:26:48 -0400

    base home component

---
+++ components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { ScrollView, Container, VStack, Text } from "native-base";
+
+const Home = () => {
+  return (
+    <ScrollView>
+      <VStack alignItems="center">
+        <Container></Container>
+      </VStack>
+    </ScrollView>
+  );
+};
