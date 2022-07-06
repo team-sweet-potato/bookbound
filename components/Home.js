2eb63cd5413429c817bb61238dfa110a9cdc6947
Corinna Hamilton <corinnahamilton01@gmail.com>
2022-07-06 01:58:17 -0400

    basic outline of home component

--- components/Home.js
+++ components/Home.js
@@ -1,12 +1,28 @@
 import React from "react";
-import { ScrollView, Container, VStack, Text } from "native-base";
+import { ScrollView, Container, VStack, Text, Button } from "native-base";
 
 const Home = () => {
   return (
     <ScrollView>
       <VStack alignItems="center">
-        <Container></Container>
+        <Container>
+          <Content padder>
+            <Text>Hello, User!</Text>
+            <Text>
+              “I took a deep breath and listened to the old brag of my heart: I
+              am, I am, I am.” -Sylvia Plath, The Bell Jar
+            </Text>
+            <Button bordered small>
+              <Text>Currently Reading</Text>
+            </Button>
+            <Button bordered small>
+              <Text>Recommended</Text>
+            </Button>
+          </Content>
+        </Container>
       </VStack>
     </ScrollView>
   );
 };
+
+export default Home;
