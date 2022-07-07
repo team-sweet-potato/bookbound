c817bb2359cf3f6c77faa787d055bd66c6e274fc
Noelle Mittelstaedt <noellemittelstaedt@gmail.com>
2022-07-07 10:08:23 -0400

    Saved UserProfile again

--- components/UserProfile.js
+++ components/UserProfile.js
@@ -27,62 +27,59 @@ const UserProfile = () => {
         </Stack>
       </Flex>
       <BBLogo />
-        <Flex direction="row" mb="2" mt="1.5">
-          <Box safeAreaTop>
-            <VStack space={2.5} w="100%" px="3">
-              <Avatar
-                size="lg"
-                source={{
-                  uri: "https://media.istockphoto.com/photos/man-sitting-on-sofa-reading-a-book-picture-id457207689?k=20&m=457207689&s=612x612&w=0&h=UNc95C-PqL_bXVz20Xq_c5RlIRen2RCc1avDlQKs-VA=",
-                }}
-              ></Avatar>
-            </VStack>
-          </Box>
-        </Flex>
-        <Container>
-          <Box space={4} w="100%" px="3" padding="3">
-            <Text fontSize="lg" fontWeight="medium">
-              Gary the Test Guy
-            </Text>
-            <Text fontSize="sm" fontWeight="medium">
-              @gary_loves_books
-            </Text>
-            <Text fontSize="md" fontWeight="medium">
-              Brooklyn, NY
-            </Text>
-            <Text>some more user data</Text>
-          </Box>
-        </Container>
-          <Divider my="2" />
-          <Center>
-            <Text fontSize="lg" fontWeight="medium" padding="3">
-              Gary's Stacks
-            </Text>
+      <Flex direction="row" mb="2" mt="1.5">
+        <Box safeAreaTop>
+          <VStack space={2.5} w="100%" px="3">
+            <Avatar
+              size="lg"
+              source={{
+                uri: "https://media.istockphoto.com/photos/man-sitting-on-sofa-reading-a-book-picture-id457207689?k=20&m=457207689&s=612x612&w=0&h=UNc95C-PqL_bXVz20Xq_c5RlIRen2RCc1avDlQKs-VA=",
+              }}
+            ></Avatar>
+          </VStack>
+        </Box>
+      </Flex>
+      <Container>
+        <Box space={4} w="100%" px="3" padding="3">
+          <Text fontSize="lg" fontWeight="medium">
+            Gary the Test Guy
+          </Text>
+          <Text fontSize="sm" fontWeight="medium">
+            @gary_loves_books
+          </Text>
+          <Text fontSize="md" fontWeight="medium">
+            Brooklyn, NY
+          </Text>
+          <Text>some more user data</Text>
+        </Box>
+      </Container>
+      <Divider my="2" />
+      <Center>
+        <Text fontSize="lg" fontWeight="medium" padding="3">
+          Gary's Stacks
+        </Text>
+      </Center>
+      <ScrollView horizontal={true}>
+        <HStack space={3} justifyContent="center" mb="2" mt="1.5">
+          <Center h="40" w="20" bg="#cb997e" rounded="md" shadow={3}>
+            Romance
           </Center>
-          <ScrollView horizontal={true}>
-
-          <HStack space={3} justifyContent="center" mb="2" mt="1.5">
-            <Center h="40" w="20" bg="#cb997e" rounded="md" shadow={3}>
-              Romance
-            </Center>
-            <Center h="40" w="20" bg="#ddbea9" rounded="md" shadow={3}>
-              Sci Fi
-            </Center>
-            <Center h="40" w="20" bg="#ffe8d6" rounded="md" shadow={3}>
-              Fantasy
-            </Center>
-            <Center h="40" w="20" bg="#b7b7a4" rounded="md" shadow={3}>
-              Horror
-            </Center>
-            <Center h="40" w="20" bg="#a5a58d" rounded="md" shadow={3}>
-              Adventure
-            </Center>
-            <Center h="40" w="20" bg="#6b705c" rounded="md" shadow={3}>
-              History
-            </Center>
-          </HStack>
-
-
+          <Center h="40" w="20" bg="#ddbea9" rounded="md" shadow={3}>
+            Sci Fi
+          </Center>
+          <Center h="40" w="20" bg="#ffe8d6" rounded="md" shadow={3}>
+            Fantasy
+          </Center>
+          <Center h="40" w="20" bg="#b7b7a4" rounded="md" shadow={3}>
+            Horror
+          </Center>
+          <Center h="40" w="20" bg="#a5a58d" rounded="md" shadow={3}>
+            Adventure
+          </Center>
+          <Center h="40" w="20" bg="#6b705c" rounded="md" shadow={3}>
+            History
+          </Center>
+        </HStack>
       </ScrollView>
     </NativeBaseProvider>
   );
