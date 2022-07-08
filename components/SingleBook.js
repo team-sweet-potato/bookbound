a1621cd476288ad7852f8e8652b957a25cf6b313
Beth Brennan <bethbrennan2@gmail.com>
2022-07-07 22:57:49 -0400

    added firebase functionality to SingleBook component

--- components/SingleBook.js
+++ components/SingleBook.js
@@ -1,64 +1,123 @@
-import { ScrollView, Container, Image, VStack, Text, Heading, Badge, Icon, Row, Divider, Fab } from "native-base";
+import { ScrollView, Container, Image, VStack, Text, Heading, Badge, Icon, Row, Divider, Fab, Center, Box, Select, CheckIcon } from "native-base";
 import React, { useEffect, useState } from "react";
 import { FontAwesome, AntDesign } from '@expo/vector-icons';
 import axios from 'axios';
+import { auth, db } from '../firebase';
+import { collection, getDocs, doc, updateDoc, deleteDoc, setDoc } from "firebase/firestore";
 
 
 const SingleBook = ({ route }) => {
   const [book, setBook] = useState({});
+  const [list, setList] = useState('');
+  const [rating, setRating] = useState(0);
+  const isbn = route.params.isbn;
 
   const fetchBook = async () => {
     try  {
-      // const {data} = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=isbn:${route.params.isbn}`);
-      const {data} = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=isbn:9781250313218`);
-      console.log('data', data)
-      setBook(data);
+      const {data} = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`);
+      if (data) {
+        setBook(data.items[0].volumeInfo);
+      }
     }
     catch (err) {
       console.log(err);
     }
   }
 
+  const fetchList = async () => {
+    if (list === '') {
+      const querySnapshotRead = await getDocs(collection(doc(db, "users", auth.currentUser.uid), "readBooks"));
+      querySnapshotRead.forEach((doc) => {
+        if (doc.id === isbn) {
+          setRating(doc.data().rate)
+          setList("readBooks")
+        }
+      });
+    }
+
+    if (list === '') {
+      const querySnapshotReading = await getDocs(collection(doc(db, "users", auth.currentUser.uid), "currentlyReading"));
+      querySnapshotReading.forEach((doc) => {
+        if (doc.id === isbn) {
+          setList("currentlyReading")
+        }
+      });
+    }
+
+    if (list === '') {
+      const querySnapshotToRead = await getDocs(collection(doc(db, "users", auth.currentUser.uid), "toReadBooks"));
+      querySnapshotToRead.forEach((doc) => {
+        if (doc.id === isbn) {
+          setList("toReadBooks")
+        }
+      });
+    }
+  }
+
+  const handlePress = async (index) => {
+    setRating(index);
+    await updateDoc(doc(doc(db, "users", auth.currentUser.uid), "readBooks", isbn), {rate: index});
+  }
+
+  const handleValueChange = async (value) => {
+    if (list !== '') {
+      await deleteDoc(doc(doc(db, "users", auth.currentUser.uid), list, isbn));
+    }
+    await setDoc(doc(doc(db, "users", auth.currentUser.uid), value, isbn), {});
+    setList(value);
+  }
+
   useEffect(() => {
     fetchBook();
+    fetchList();
   }, [])
 
   return (
-    !book.items ? (<Text>Loading</Text>) : (
+    Object.keys(book).length === 0 ? (<Text>Loading</Text>) : (
     <ScrollView>
       <VStack space={4} alignItems="center">
         <Container>
-          <Badge
-            colorScheme="success" rounded="full" mb={-4} mr={-4} zIndex={1} variant="solid" alignSelf="flex-end" _text={{
-            fontSize: 12
-          }}>
-            Read
-          </Badge>
+          {list === "readBooks" && <Badge colorScheme="success" rounded="full" mb={-4} mr={-4} zIndex={1} variant="solid" alignSelf="flex-end" _text={{fontSize: 12}}>Read</Badge>}
+          {list === "currentlyReading" && <Badge colorScheme="info" rounded="full" mb={-4} mr={-4} zIndex={1} variant="solid" alignSelf="flex-end" _text={{fontSize: 12}}>Reading</Badge>}
+          {list === "toReadBooks" && <Badge colorScheme="warning" rounded="full" mb={-4} mr={-4} zIndex={1} variant="solid" alignSelf="flex-end" _text={{fontSize: 12}}>To Read</Badge>}
           <Image source={{
-          uri: book.items[0].volumeInfo.imageLinks.thumbnail
-          }} alt={`${book.items[0].volumeInfo.title} book cover`} size="2xl" />
+          uri: book.imageLinks.thumbnail
+          }} alt={`${book.title} book cover`} size="2xl" />
         </Container>
         <Container>
-          <Heading size="xl">{book.items[0].volumeInfo.title}</Heading>
-          <Heading size="md">By {book.items[0].volumeInfo.authors.join(", ")}</Heading>
+          <Heading size="xl">{book.title}</Heading>
+          <Heading size="md">By {book.authors.join(", ")}</Heading>
           <Divider my="2" _light={{
               bg: "muted.800"
             }} _dark={{
               bg: "muted.50"
             }} />
-          <Text fontSize="sm">{book.items[0].volumeInfo.description}</Text>
+          <Text fontSize="sm">{book.description}</Text>
         </Container>
+        {list === "readBooks" ? (
         <Container>
           <Heading size="sm">Your Rating:</Heading>
           <Row>
-            <Icon as={FontAwesome} name="star" />
-            <Icon as={FontAwesome} name="star" />
-            <Icon as={FontAwesome} name="star" />
-            <Icon as={FontAwesome} name="star" />
-            <Icon as={FontAwesome} name="star-o" />
+            <Icon as={FontAwesome} name={rating >= 1 ? "star" : "star-o" } onPress={() => handlePress(1)} />
+            <Icon as={FontAwesome} name={rating >= 2 ? "star" : "star-o" } onPress={() => handlePress(2)} />
+            <Icon as={FontAwesome} name={rating >= 3 ? "star" : "star-o" } onPress={() => handlePress(3)} />
+            <Icon as={FontAwesome} name={rating >= 4 ? "star" : "star-o" } onPress={() => handlePress(4)} />
+            <Icon as={FontAwesome} name={rating === 5 ? "star" : "star-o" } onPress={() => handlePress(5)} />
           </Row>
         </Container>
-        <Fab renderInPortal={false} shadow={2} size="sm" icon={<Icon color="white" as={AntDesign} name="plus" size="sm" />} />
+        ): <Text></Text>}
+        <Center>
+          <Box w="3/4" maxW="300">
+            <Select selectedValue={list} minWidth="200" accessibilityLabel="Add to List" placeholder="Add to List" _selectedItem={{
+            bg: "teal.600",
+            endIcon: <CheckIcon size="5" />
+            }} mt={1} onValueChange={itemValue => handleValueChange(itemValue)}>
+              <Select.Item label="Read" value="readBooks" />
+              <Select.Item label="To Be Read" value="toReadBooks" />
+              <Select.Item label="Currently Reading" value="currentlyReading" />
+            </Select>
+          </Box>
+        </Center>
       </VStack>
     </ScrollView>)
   )
