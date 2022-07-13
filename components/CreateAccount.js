0c2e3b46c3f91cc092f5c50151fd0d891c823486
Beth Brennan <bethbrennan2@gmail.com>
2022-07-13 16:27:34 -0400

    login and create account error handling

--- components/CreateAccount.js
+++ components/CreateAccount.js
@@ -15,8 +15,10 @@ import {
   ScrollView,
   Text,
   VStack,
+  WarningOutlineIcon
 } from "native-base";
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { Alert } from 'react-native';
 
 const CreateAccount = ({ navigation }) => {
   // Variables to sign up
@@ -26,10 +28,35 @@ const CreateAccount = ({ navigation }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [verifyPassword, setVerifyPassword] = useState("");
+  const [errors, setErrors] = useState({email: "", password: "", firstName: "", lastName: "", username: ""});
+
+  const validate = () => {
+    if (email === "") {
+      setErrors({...errors, email: "Please enter a valid email."});
+    }
+    if (password === "" || verifyPassword === "") {
+      setErrors({...errors, password: "Please enter a password."});
+    } else if (password !== verifyPassword) {
+      setErrors({...errors, password: "Passwords do not match."});
+    }
+    if (firstName === "") {
+      setErrors({...errors, firstName: "Please enter your first name."});
+    }
+    if (lastName === "") {
+      setErrors({...errors, lastName: "Please enter your last name."});
+    }
+    if (username === "") {
+      setErrors({...errors, username: "Please enter a valid username."});
+    }
+    if (errors.email || errors.password || errors.firstName || errors.lastName || errors.username) {
+      return false
+    }
+    return true;
+  };
 
   async function handleSignUp() {
-    if (password === verifyPassword && firstName && lastName && username) {
-      try {
+    try {
+      if (validate()) {
         await createUserWithEmailAndPassword(auth, email, password);
         await setDoc(doc(db, "users", auth.currentUser.uid), {
           firstName: firstName,
@@ -37,9 +64,9 @@ const CreateAccount = ({ navigation }) => {
           username: username,
         });
         navigation.push("Nav Bar");
-      } catch (error) {
-        console.log(error);
       }
+    } catch (error) {
+      Alert.alert("Sign up failed", "Please try again.");
     }
   }
 
@@ -78,55 +105,79 @@ const CreateAccount = ({ navigation }) => {
               Create an account to continue!
             </Heading>
             <VStack space={3} mt="5">
-              <FormControl>
+              <FormControl isRequired isInvalid={!!errors.firstName}>
                 <FormControl.Label>First Name</FormControl.Label>
                 <Input
                   value={firstName}
                   placeholder="firstName"
-                  onChangeText={(text) => setFirstName(text)}
+                  onChangeText={(text) => {
+                    setFirstName(text)
+                    setErrors({...errors, firstName: ""})
+                  }}
                 />
+                {errors.firstName && <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>{errors.firstName}</FormControl.ErrorMessage>}
               </FormControl>
-              <FormControl>
+              <FormControl isRequired isInvalid={!!errors.lastName}>
                 <FormControl.Label>Last Name</FormControl.Label>
                 <Input
                   value={lastName}
                   placeholder="lastName"
-                  onChangeText={(text) => setLastName(text)}
+                  onChangeText={(text) => {
+                    setLastName(text)
+                    setErrors({...errors, lastName: ""})
+                  }}
                 />
+                {errors.lastName && <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>{errors.lastName}</FormControl.ErrorMessage>}
               </FormControl>
-              <FormControl>
+              <FormControl isRequired isInvalid={!!errors.username}>
                 <FormControl.Label>Username</FormControl.Label>
                 <Input
                   value={username}
                   placeholder="username"
-                  onChangeText={(text) => setUsername(text)}
+                  onChangeText={(text) => {
+                    setUsername(text)
+                    setErrors({...errors, username: ""})
+                  }}
                 />
+                {errors.username && <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>{errors.username}</FormControl.ErrorMessage>}
               </FormControl>
-              <FormControl>
+              <FormControl isRequired isInvalid={!!errors.email}>
                 <FormControl.Label>Email</FormControl.Label>
                 <Input
                   value={email}
                   placeholder="email"
-                  onChangeText={(text) => setEmail(text)}
+                  onChangeText={(text) => {
+                    setEmail(text)
+                    setErrors({...errors, email: ""})
+                  }}
                 />
+                {errors.email && <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>{errors.email}</FormControl.ErrorMessage>}
               </FormControl>
-              <FormControl>
+              <FormControl isRequired isInvalid={!!errors.password}>
                 <FormControl.Label>Password</FormControl.Label>
                 <Input
                   value={password}
                   placeholder="password"
-                  onChangeText={(text) => setPassword(text)}
+                  onChangeText={(text) => {
+                    setPassword(text)
+                    setErrors({...errors, password: ""})
+                  }}
                   secureTextEntry
                 />
+                {errors.password && <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>{errors.password}</FormControl.ErrorMessage>}
               </FormControl>
-              <FormControl>
+              <FormControl isRequired isInvalid={!!errors.password}>
                 <FormControl.Label>Verify Password</FormControl.Label>
                 <Input
                   value={verifyPassword}
                   placeholder="password"
-                  onChangeText={(text) => setVerifyPassword(text)}
+                  onChangeText={(text) => {
+                    setVerifyPassword(text)
+                    setErrors({...errors, password: ""})
+                  }}
                   secureTextEntry
                 />
+                {errors.password && <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>{errors.password}</FormControl.ErrorMessage>}
               </FormControl>
               <Button mt="2" colorScheme="indigo" onPress={handleSignUp}>
                 Sign up
--- components/Login.js
+++ components/Login.js
@@ -14,20 +14,53 @@ import {
   ScrollView,
   Text,
   VStack,
+  WarningOutlineIcon
 } from "native-base";
+import { Alert } from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import axios from "axios";
 
 const Login = ({ navigation }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  const validate = () => {
+    if (email === "") {
+      setEmailError("Please enter a valid email.");
+    }
+    if (password === "") {
+      setPasswordError("Please enter your password.");
+    }
+    if (emailError || passwordError) {
+      return false
+    }
+    return true;
+  };
 
   async function handleLogin() {
     try {
-      await signInWithEmailAndPassword(auth, email, password);
-      navigation.push("Nav Bar");
+      if (validate()) {
+        await signInWithEmailAndPassword(auth, email, password);
+        navigation.push("Nav Bar");
+      }
     } catch (error) {
-      console.log(error);
+      let alertTitle = "";
+      let alertMessage = "";
+
+      if (error.code === "auth/too-many-requests") {
+        alertTitle = "Too many login attempts";
+        alertMessage = "Please try again later.";
+      } else if (error.code === "auth/wrong-password") {
+        alertTitle = "Incorrect username/password";
+        alertMessage = "Please try again.";
+      } else {
+        alertTitle = "Login failed";
+        alertMessage = "Please try again later.";
+      }
+
+      Alert.alert(alertTitle, alertMessage);
     }
   }
 
@@ -70,22 +103,30 @@ const Login = ({ navigation }) => {
                 Sign in to continue!
               </Heading>
               <VStack space={3} mt="5">
-                <FormControl>
-                  <FormControl.Label>Email ID</FormControl.Label>
+                <FormControl isRequired isInvalid={emailError}>
+                  <FormControl.Label>Email</FormControl.Label>
                   <Input
                     value={email}
                     placeholder="email"
-                    onChangeText={(text) => setEmail(text)}
+                    onChangeText={(text) => {
+                      setEmail(text)
+                      setEmailError("")
+                    }}
                   />
+                  {emailError && <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>{emailError}</FormControl.ErrorMessage>}
                 </FormControl>
-                <FormControl>
+                <FormControl isRequired isInvalid={passwordError}>
                   <FormControl.Label>Password</FormControl.Label>
                   <Input
                     value={password}
                     placeholder="password"
-                    onChangeText={(text) => setPassword(text)}
+                    onChangeText={(text) => {
+                      setPassword(text)
+                      setPasswordError("")
+                    }}
                     secureTextEntry
                   />
+                  {passwordError && <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>{passwordError}</FormControl.ErrorMessage>}
                 </FormControl>
                 <Button mt="2" colorScheme="indigo" onPress={handleLogin}>
                   Sign in
